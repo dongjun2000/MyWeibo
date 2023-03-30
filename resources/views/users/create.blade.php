5789a5aad79861b5d8673ab1b8e3d5aeb1e04846
Dongjun <dongjun@246ha.com>
2023-03-30 10:09:39 +0800

    增加 CSRF 验证

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -10,6 +10,8 @@
       </div>
       <div class="card-body">
         <form action="{{ route('users.store') }}" method="post">
+          @csrf
+
           <div class="mb-3">
             <label for="name">名称：</label>
             <input type="text" name="name" class="form-control" value="{{ old('name') }}">
